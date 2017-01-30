4bcab74a78c5b73e053a2931b1b51c3095672999
arafat25 <auddullah1@student.gsu.edu>
2017-01-30 18:42:17 -0500

    Jan 30 new

--- app/src/main/java/com/example/arafat/arafatdemo/ListViewActivity.java
+++ app/src/main/java/com/example/arafat/arafatdemo/ListViewActivity.java
@@ -2,11 +2,14 @@ package com.example.arafat.arafatdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.arafat.arafatdemo.adapter.ListViewAdapter;
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView listView;
 
     @Override
@@ -22,5 +25,11 @@ public class ListViewActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list_view);
         ListViewAdapter listViewAdapter = new ListViewAdapter(this);
         listView.setAdapter(listViewAdapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, "listView was clicked at position: " + position,Toast.LENGTH_LONG).show();
     }
 }
