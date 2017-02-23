4a9e4b93557eb82849bc2fc90327ca59ee72ea2e
arafat25 <auddullah1@student.gsu.edu>
2017-02-22 19:47:24 -0500

    updated classwork
    2/22/17 classwork

--- app/src/main/java/com/example/arafat/arafatdemo/MainActivity.java
+++ app/src/main/java/com/example/arafat/arafatdemo/MainActivity.java
@@ -19,107 +19,104 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class MainActivity extends BaseActivity {
+    public class MainActivity extends BaseActivity {
+        private ImageButton bt1;
+        private ImageButton bt3;
+        private ImageButton bt4;
 
-    private ImageButton bt1;
-    private ImageButton bt3;
-
-    @OnClick(R.id.bt2)
-    public void button2Click() {
-        Intent intent = new Intent(this,DialogActivity.class);
-        startActivityForResult(intent,2);
-
-
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initialView();
-        initialListener();
-        ButterKnife.bind(this);
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        toastShort("onStart");
-    }
-
-    private void initialView() {
-
-        bt1 = (ImageButton) findViewById(R.id.bt1);
-        bt3 = (ImageButton) findViewById(R.id.bt3);
-
-    }
+        @OnClick(R.id.bt2)
+        public void button2Click(){
+            Intent intent = new Intent(this, DialogActivity.class);
+            startActivityForResult(intent, 2);
+            //toActivity(DialogActivity.class);
+        }
 
 
 
-    private void initialListener()  {
-//bt1.setOnClickListener(new View.OnClickListener() {
-        //@Override
-        //public void onClick (View v) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            initialView();
+            initialListener();
+            ButterKnife.bind(this);
+        }
 
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  Toast.makeText(view.getContext(), "Button2 was clicked", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(view.getContext(),ViewPagerActivity.class);
-                intent.putExtra("key", "value");
-                Bundle bundle = new Bundle();
-                bundle.putInt("Integer", 12345);
-                Book book = new Book();
-                book.setName("Android");
-                book.setAuthor("Arafat");
-                bundle.putSerializable("book", book);
-                intent.putExtras(bundle);
-                startActivityForResult(intent, 1);
+        private void initialView(){
+            bt1 = (ImageButton) findViewById(R.id.bt1);
+            bt3 = (ImageButton) findViewById(R.id.bt3);
+            bt4 = (ImageButton) findViewById(R.id.bt4);
 
+        }
+
+        private void initialListener(){
+            bt1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "Button1 was clicked", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(v.getContext(),ViewPagerActivity.class);
+                    intent.putExtra("key","value");
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("Integer", 12345);
+                    Book book = new Book();
+                    book.setName("Android");
+                    book.setAuthor("Laura");
+                    bundle.putSerializable("book", book);
+                    intent.putExtras(bundle);
+                    startActivityForResult(intent, 1);
+                }
+            });
+            bt3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    //toActivity(ListViewActivity.class);
+                    Intent intent = new Intent(v.getContext(),ListViewActivity.class);
+                    startActivityForResult(intent, 3);
+                }
+            });
+            bt4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    //toActivity(ListViewActivity.class);
+                    Intent intent = new Intent(v.getContext(),ActivityA.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            switch (requestCode){
+                case 1:
+                    String message = data.getStringExtra("message");
+                    toastShort(message);
+                    break;
+                case 2:
+                    toastShort("Dialog");
+                    break;
+                case 3:
+
+                    toastShort("ListView");
+                    break;
+                default:
             }
-        });
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), ListViewActivity.class);
-                startActivityForResult(intent,3);
-/*
-                Intent intent = new Intent(view.getContext(), ListViewActivity.class);
-                startActivity(intent);
-*/
-            }
-        });
-    }
+        }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case 1:
-                String message = data.getStringExtra("message");
-                toastShort(message);
-                break;
-            case 2:
-                toastShort("Dialog");
-                break;
-            case 3:
-                toastShort("ListView");
-                break;
-            default:
+        public void onClick(View v){
+            toastLong("Button2 was clicked");
+            utilLog.logD("testD","Toast");
+
+//        Toast.makeText(this, "Button2 was clicked", Toast.LENGTH_LONG).show();
+//        Log.d("testD","Toast1");
+        }
+
+        @Override
+        protected void onStart() {
+            toastShort("onStart");
+            super.onStart();
         }
     }
-
-    public void onClick(View view) {
-        //Toast.makeText(this,"listView was clicked at position: ",Toast.LENGTH_LONG).show();
-        toastLong("Button2 was clicked");
-        //key = your class name
-        utilLog.logD("testD", "Toast");
-        //Log.e("testE", "Toast");
-        //Log.i("testI", "Toast");
-        //Log.v("testI", "Toast");
-        //Log.w("testI", "Toast");
-
-    }
-
-
-}
