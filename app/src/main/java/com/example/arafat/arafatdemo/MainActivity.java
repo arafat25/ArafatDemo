ff06d8a836c0610172ac3d10c3d6f1a6b18b7c3a
arafat25 <auddullah1@student.gsu.edu>
2017-03-06 18:42:54 -0500

    3/6/17
    animator

--- app/src/main/java/com/example/arafat/arafatdemo/MainActivity.java
+++ app/src/main/java/com/example/arafat/arafatdemo/MainActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -16,19 +20,29 @@ import com.example.arafat.arafatdemo.util.utilLog;
 import java.util.List;
 import java.util.Timer;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-    public class MainActivity extends BaseActivity {
+    public class MainActivity extends BaseActivity implements View.OnTouchListener{
         private ImageButton bt1;
         private ImageButton bt3;
         private ImageButton bt4;
+        private ScaleGestureDetector mGestureDetector;
+
+        @BindView(R.id.main_fl) FrameLayout fl;
+
 
 
         @OnClick(R.id.main_anim_bt)
         public void toAnimation(){
             toActivity(AnimationActivity.class);
+        }
+
+        @OnClick(R.id.animator_bt)
+        public void toAnimator(){
+            toActivity(AnimatorActivity.class);
         }
 
         @OnClick(R.id.bt4)
@@ -57,6 +71,8 @@ import butterknife.OnClick;
             initialView();
             initialListener();
             ButterKnife.bind(this);
+            mGestureDetector = new ScaleGestureDetector(this, new simpleGestureListener());
+            fl.setOnTouchListener(this);
         }
 
         private void initialView(){
@@ -130,6 +146,92 @@ import butterknife.OnClick;
 
 //        Toast.makeText(this, "Button2 was clicked", Toast.LENGTH_LONG).show();
 //        Log.d("testD","Toast1");
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event){
+            return mGestureDetector.onTouchEvent(event);
+        }
+
+        private class simpleGestureListener extends
+                GestureDetector.SimpleOnGestureListener implements ScaleGestureDetector.OnScaleGestureListener {
+
+            public boolean onDown(MotionEvent e){
+                utilLog.logD("MyGesture", "onDown");
+                toastShort( "onDown");
+                return false;
+            }
+
+            public void onShowPress(MotionEvent e) {
+                utilLog.logD("MyGesture", "onShowPress");
+                toastShort( "onShowPress");
+
+            }
+
+            public void onLongPress(MotionEvent e) {
+                utilLog.logD("MyGesture", "onLongPress");
+                toastShort( "onLongPress");
+
+            }
+
+            public boolean onSingleTapUp(MotionEvent e) {
+                utilLog.logD("MyGesture", "onSingleTapUp");
+                toastShort( "onSingleTapUp");
+                return true;
+            }
+
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                utilLog.logD("MyGesture", "onSingleTapUp");
+                toastShort( "onSingleTapConfirmed");
+                return true;
+            }
+
+            public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                    float distanceX, float distanceY) {
+                utilLog.logD("MyGesture", "onScroll:" + (e2.getX() - e1.getX())+ " " + distanceX);
+                toastShort( "onScroll");
+                return true;
+            }
+
+            public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                    float velocityX, float velocityY) {
+                utilLog.logD("MyGesture", "onFling");
+                toastShort( "onFling");
+                return true;
+            }
+            
+            public boolean onDoubleTap(MotionEvent e) {
+       //         utilLog.logD("MyGesture", "onDoubleTap");
+                toastShort( "onDoubleTap");
+                return true;
+            }
+
+            public boolean onDoubleTapEvent(MotionEvent e) {
+    //            utilLog.logD("MyGesture", "onDoubleTapEvent");
+                toastShort( "onDoubleTapEvent");
+                return true;
+            }
+
+
+
+
+            @Override
+            public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+                return false;
+            }
+
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+                return false;
+            }
+
+            @Override
+            public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+
+            }
+
+
+
         }
 
         @Override
